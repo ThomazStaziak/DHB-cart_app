1f2868919b0147b41ae3299580a4d0649719bca8
ThomazStaziak <thostaziak@gmail.com>
2019-06-21 13:07:35 -0300

    adding routes to the quantity

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ const upload = multer({ dest: './' })
 const routes = new express.Router()
 
 routes.get('/produtos', cartController.index)
-routes.post('/adicionar-produtos', upload.single('bla'), cartController.store)
+routes.post('/adicionar-produtos', upload.single('optmize'), cartController.store)
+routes.get('/aumentar-quantidade/:id', upload.single('optmize'), cartController.increaseQuantity)
+routes.get('/diminuir-quantidade/:id', upload.single('optmize'), cartController.decreaseQuantity)
 
 module.exports = routes
