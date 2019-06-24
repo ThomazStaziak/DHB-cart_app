9a2f4a3cfef5d59e08513e88bfe44801afb66acf
ThomazStaziak <thostaziak@gmail.com>
2019-06-24 18:13:30 -0300

    adding timestamps to the cart model

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose')
 
 const CartSchema = new mongoose.Schema({
     nome: String,
-    valor: Number,
+    valor: {
+        type: Number,
+        default: 30
+    },
     quantidade: {
         type: Number,
         default: 1
     }
+}, 
+{
+    timestamps: true
 })
 
 module.exports = mongoose.model('Cart', CartSchema)
