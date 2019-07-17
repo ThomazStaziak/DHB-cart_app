246f355ad1ad690db54b6e73646955d8d348475a
ThomazStaziak <thostaziak@gmail.com>
2019-07-17 09:03:13 -0300

    optimizing the routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,11 @@
-const express = require('express')
-const multer = require('multer')
+const express = require("express");
+const cartController = require("./controllers/cartController");
 
-const cartController = require('./controllers/cartController')
+const routes = new express.Router();
 
-/* NEEDING OPTMIZE */
-const upload = multer({ dest: './' })
-const routes = new express.Router()
+routes.get("/produtos", cartController.index);
+routes.post("/adicionar-produtos", cartController.store);
+routes.get("/aumentar-quantidade/:id", cartController.increaseQuantity);
+routes.get("/diminuir-quantidade/:id", cartController.decreaseQuantity);
 
-routes.get('/produtos', cartController.index)
-routes.post('/adicionar-produtos', upload.single('optmize'), cartController.store)
-routes.get('/aumentar-quantidade/:id', upload.single('optmize'), cartController.increaseQuantity)
-routes.get('/diminuir-quantidade/:id', upload.single('optmize'), cartController.decreaseQuantity)
-
-module.exports = routes
+module.exports = routes;
