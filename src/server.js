6c6caebc6fa2af4c0ae5565c8a291eae9914e577
ThomazStaziak <thostaziak@gmail.com>
2019-07-17 09:03:24 -0300

    optimizing the server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-require("dotenv/config")
+require("dotenv/config");
 
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express()
-const server = require('http').Server(app)
+const app = express();
+const server = require("http").Server(app);
 
-const env = process.env
+const env = process.env;
 
 mongoose.connect(`mongodb+srv://${env.DB_USER}:${env.DB_PASS}@${env.DB_CONN}`, {
-    useNewUrlParser: true
-})
+  useNewUrlParser: true
+});
 
-app.use(cors())
-app.use(require('./routes'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(require("./routes"));
 
-server.listen(env.PORT)
+server.listen(env.PORT);
